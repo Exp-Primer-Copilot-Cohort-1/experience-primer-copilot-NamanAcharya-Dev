81a7a87bc2df564f2ca33d839a40b0413a88b047
Naman Acharya <namanacharya245@gmail.com>
2024-03-10 06:01:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  return {
+    member: {
+      name: 'John',
+      age: 30,
+      skills: {
+        programming: {
+          languages: ['JavaScript', 'Python', 'Java', 'C#'],
+          frontend: ['React', 'Vue', 'Angular'],
+          backend: ['Node', 'Express', 'Django', 'Spring'],
+        },
+        devops: {
+          tools: ['Docker', 'Kubernetes', 'Jenkins', 'Ansible'],
+          cloud: ['AWS', 'Azure', 'GCP', 'Digital Ocean'],
+        },
+      },
+    },
+  };
+}
